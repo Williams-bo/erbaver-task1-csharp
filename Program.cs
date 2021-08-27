f2f541345bf1ee77c315fb33265b2b60823d63c1
Williams-bo <williamssunday170@gmail.com>
2021-08-27 01:47:11 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+            int result = 0;
+
+            // first number
+            Console.Write("Please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            // second number
+            Console.Write("Please enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            // third number
+            Console.Write("Please enter the third number: ");
+            thirdNumber = int.Parse(Console.ReadLine());
+
+            // Task 1 - Multiplication
+            result = firstNumber * secondNumber * thirdNumber;
+            Console.WriteLine("The answer is: " + result);
+
+
+        }
+    }
+}
